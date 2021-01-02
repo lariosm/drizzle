a44cf14187cbb6949c2e62e1d12227dbcbe14526
Manuel Larios <manuellarios5@gmail.com>
2021-01-01 18:04:22 -0800

    Added scheduling function to program and finalized requirements needed for deployment

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ import os
 from parser import phoneMessage
 from weather_email import sendMessage
 from getData import getWeatherData
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 api_key = os.getenv('API_KEY')
 sender_email = os.getenv('FROM_EMAIL')
-receiver_email = os.getenv('SEND_EMAIL') 
+receiver_email = os.getenv('SEND_EMAIL')
 password = os.getenv('PASSWORD')
 
-data = getWeatherData(api_key)
-premessage = """Subject: Weather Update (Baltazar Ortiz)
+task = BlockingScheduler()
 
-"""
-message = phoneMessage(data, premessage)
-sendMessage(message, sender_email, receiver_email, password)
+
+@task.scheduled_job('interval', minutes=3)
+def send_email():
+    data = getWeatherData(api_key)
+    premessage = """Subject: Weather Update (Baltazar Ortiz)
+
+    """
+    message = phoneMessage(data, premessage)
+    print(message)
+    sendMessage(message, sender_email, receiver_email, password)
