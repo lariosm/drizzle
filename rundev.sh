7852d7f4c714226ba9fb694a9ca8c41218aeddf7
Manuel Larios <manuellarios5@gmail.com>
2021-01-01 16:15:37 -0800

    Updated rundev.sh

--- rundev.sh
+++ rundev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -a
-. ./settings.env  # Loads environment variables from .env file
+. ./.env  # Loads environment variables from .env file
 set +a
 
 python main.py  # Runs the program
