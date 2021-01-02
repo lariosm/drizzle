a242b08d8007b6032003c3ebfb40a6be14c4afca
Manuel Larios <manuellarios5@gmail.com>
2021-01-01 20:15:13 -0800

    Scheduled emails revolve around UTC time

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ from parser import phoneMessage
 from weather_email import sendMessage
 from getData import getWeatherData
 from apscheduler.schedulers.blocking import BlockingScheduler
+from pytz import utc
 
 api_key = os.getenv('API_KEY')
 sender_email = os.getenv('FROM_EMAIL')
 receiver_email = os.getenv('SEND_EMAIL')
 password = os.getenv('PASSWORD')
 
-task = BlockingScheduler()
+task = BlockingScheduler(timezone=utc)
 
 
-@task.scheduled_job('interval', minutes=3)
+# Sends out an email every day at 17:30 (9:30 AM Pacific time)
+@task.scheduled_job('cron', hour=17, minute=30)
 def send_email():
     data = getWeatherData(api_key)
     premessage = """Subject: Weather Update (Baltazar Ortiz)
 
     """
-    message = phoneMessage(data, premessage)
-    print(message)
     sendMessage(message, sender_email, receiver_email, password)
 
 
